85944d91923b444d8429ea1ec023af67094fb320
Pablo28 <burberking9@gmail.com>
2025-11-13 18:53:25 -0600

    First stage

--- src/main/java/frc/robot/subsystems/mechanisms/Elevator.java
+++ src/main/java/frc/robot/subsystems/mechanisms/Elevator.java
@@ -21,43 +21,43 @@ import frc.robot.Constants;
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
   private final TalonFX m_leftElevator;
-  private final TalonFX m_rightElevator;
+  //private final TalonFX m_rightElevator;
   public PositionVoltage position = new PositionVoltage(0);
   public VelocityVoltage velocityControl = new VelocityVoltage(0);
-  private final Follower followControl;
+  //private final Follower followControl;
   public TalonFXConfiguration elevatorConfig = new TalonFXConfiguration();
   public TalonFXConfiguration elevatorConfigSlow = new TalonFXConfiguration();
   public Slot0Configs elevatorConfigPIDUp = elevatorConfig.Slot0;
   public Slot1Configs elevatorConfigPIDDown = elevatorConfig.Slot1;
-  public boolean elevatorSlot0 = true;
+   public boolean elevatorSlot0 = true;
   public Elevator() {
-    elevatorConfig.Voltage.PeakForwardVoltage = 12;
-    elevatorConfig.Voltage.PeakReverseVoltage = -12;
-    elevatorConfig.TorqueCurrent.PeakForwardTorqueCurrent = 800;
-    elevatorConfig.TorqueCurrent.PeakReverseTorqueCurrent = -800;
+    // elevatorConfig.Voltage.PeakForwardVoltage = 12;
+    // elevatorConfig.Voltage.PeakReverseVoltage = -12;
+    // elevatorConfig.TorqueCurrent.PeakForwardTorqueCurrent = 800;
+    // elevatorConfig.TorqueCurrent.PeakReverseTorqueCurrent = -800;
     elevatorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
     elevatorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
     elevatorConfig.CurrentLimits.StatorCurrentLimit = 40.0;
-    elevatorConfigPIDUp.kS = 0.0; // Add 0.25 V output to overcome static friction
-    elevatorConfigPIDUp.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
-    elevatorConfigPIDUp.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
-    elevatorConfigPIDUp.kP = 1.0; // A position error of 2.5 rotations results in 12 V output
-    elevatorConfigPIDUp.kI = 0.05; // no output for integrated error
-    // elevatorConfigPIDDown.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
+    elevatorConfigPIDUp.kS = 0.1; // Add 0.25 V output to overcome static friction
+    elevatorConfigPIDUp.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    elevatorConfigPIDUp.kA = 0.11; // An acceleration of 1 rps/s requires 0.01 V output
+    elevatorConfigPIDUp.kP = 0; // A position error of 2.5 rotations results in 12 V output
+    elevatorConfigPIDUp.kI = 0; // no output for integrated error
+    // // elevatorConfigPIDDown.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
     elevatorConfigPIDDown.kP = 0.0; // A position error of 2.5 rotations results in 12 V output
     elevatorConfigPIDDown.kI = 0.0; // no output for integrated error
     elevatorConfigPIDDown.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
-    elevatorConfig.withSlot0(elevatorConfigPIDUp);
-    elevatorConfig.withSlot1(elevatorConfigPIDDown);
+    // elevatorConfig.withSlot0(elevatorConfigPIDUp);
+    // elevatorConfig.withSlot1(elevatorConfigPIDDown);
     
-    m_leftElevator = new TalonFX(Constants.SwerveConstants.kLeftElevatorMotorPort);
-    m_rightElevator = new TalonFX(Constants.SwerveConstants.kRightElevatorMotorPort);
+    m_leftElevator = new TalonFX(1); //Constants.SwerveConstants.kLeftElevatorMotorPort);
+    //m_rightElevator = new TalonFX(Constants.SwerveConstants.kRightElevatorMotorPort);
 
-    followControl = new Follower(m_leftElevator.getDeviceID(), true);
+   // followControl = new Follower(m_leftElevator.getDeviceID(), true);
 
-    m_rightElevator.getConfigurator().apply(elevatorConfig);
-    m_rightElevator.setNeutralMode(NeutralModeValue.Brake);
-    m_rightElevator.setPosition(0);
+   // m_rightElevator.getConfigurator().apply(elevatorConfig);
+   // m_rightElevator.setNeutralMode(NeutralModeValue.Brake);
+   // m_rightElevator.setPosition(0);
 
     m_leftElevator.getConfigurator().apply(elevatorConfig);
     m_leftElevator.setNeutralMode(NeutralModeValue.Brake);
@@ -69,26 +69,31 @@ public class Elevator extends SubsystemBase {
     // position.Slot = 0;
     position.Slot = 0;
     m_leftElevator.setControl(position.withEnableFOC(false).withPosition(targetPosition));
-    m_rightElevator.setControl(followControl);
+    //m_rightElevator.setControl(followControl);
   }
 
   // Takes the elevator position from all the way up to all the way down
   public void goDown() {
     velocityControl.Slot = 1;
     m_leftElevator.setControl(velocityControl.withEnableFOC(false).withVelocity(0).withFeedForward(-4).withSlot(1));
-    m_rightElevator.setControl(followControl);
+    //m_rightElevator.setControl(followControl);
+  }
+  public void setVelocity(double velocity) {
+    velocityControl.Slot = 1;
+    m_leftElevator.setControl(velocityControl.withEnableFOC(false).withVelocity(0).withFeedForward(0).withSlot(1));
+    //m_rightElevator.setControl(followControl);
   }
 
   public void resetElevatorPosition() {
     stopRotate();
     m_leftElevator.setPosition(0);
-    m_rightElevator.setPosition(0);
+  //  m_rightElevator.setPosition(0);
   }
 
   // Stops both the motors
   public void stopRotate() {
     m_leftElevator.stopMotor();
-    m_rightElevator.stopMotor();
+    //m_rightElevator.stopMotor();
   }
 
   // Gives the position of the left elevator motor
@@ -101,14 +106,14 @@ public class Elevator extends SubsystemBase {
     return Math.abs(m_leftElevator.getClosedLoopError().getValueAsDouble()) <= 1.0;
   }
 
-  public void setScoringPreset(double presetNumber, double scoringMechAngle, String presetName, double algaePresetNumber, double algaeScoringMechAngle, String algaePresetName) {
-    Constants.percentOfElevator = presetNumber;
-    Constants.scoringLevel = presetName;
-    Constants.scoringMechGoalAngle = scoringMechAngle;
-    Constants.percentOfElevatorAlgae = algaePresetNumber;
-    Constants.algaeScoringLevel = algaePresetName;
-    Constants.scoringMechGoalAngleAlgae = algaeScoringMechAngle;
-  }
+  // public void setScoringPreset(double presetNumber, double scoringMechAngle, String presetName, double algaePresetNumber, double algaeScoringMechAngle, String algaePresetName) {
+  //   Constants.percentOfElevator = presetNumber;
+  //   Constants.scoringLevel = presetName;
+  //   Constants.scoringMechGoalAngle = scoringMechAngle;
+  //   Constants.percentOfElevatorAlgae = algaePresetNumber;
+  //   Constants.algaeScoringLevel = algaePresetName;
+  //   Constants.scoringMechGoalAngleAlgae = algaeScoringMechAngle;
+  // }
 
   // Gives the position of the left elevator in motor rotations and the # the driver sets
   @Override
